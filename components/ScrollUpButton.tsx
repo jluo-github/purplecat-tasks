dc18c255e8e63be2d852cb30a58fbfdc0884cf43
jluo <jluo8@wgu.edu>
2024-02-08 18:57:45 -0800

    update

--- components/ScrollUpButton.tsx
+++ components/ScrollUpButton.tsx
@@ -5,9 +5,9 @@ import { ArrowUp } from "lucide-react";
 const ScrollUpButton = () => {
  return (
   <Button
-   className=' fixed bottom-5 right-6 z-50 cursor-pointer p-4'
-   variant='secondary'
-   size='sm'
+   className=' fixed bottom-5 right-6 z-50 cursor-pointer'
+   variant='outline'
+   size='icon'
    onClick={() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
    }}>
